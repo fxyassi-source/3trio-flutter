6000e2459044241194de5ba20ba7e59e7eeb234b
fxyassi-source <fxyassi@gmail.com>
2026-09-03 14:48:01 +0530

    Add non-negotiable 3TRIO product rules

---
+++ lib/core/app_rules.dart
@@ -0,0 +1,10 @@
+class AppRules {
+  static const int maxProfilePhotos = 5;
+  static const int freeLikesRolling24h = 100;
+  static const int minimumAge = 18;
+  static const int monthlyPremiumInr = 500;
+  static const int sixMonthPremiumInr = 2500;
+  static const int yearlyPremiumInr = 4000;
+  static const String privateLocationMessage = 'Approximate zones only';
+  static const String nearbyLikeMessage = 'Someone nearby liked you ❤️';
+}
