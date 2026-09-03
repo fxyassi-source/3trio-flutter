1e72ad32cfa67b48a769c306fda819ffb9283bc3
fxyassi-source <fxyassi@gmail.com>
2026-09-03 14:46:12 +0530

    Add 3TRIO product constants

---
+++ lib/core/app_constants.dart
@@ -0,0 +1,11 @@
+class AppConstants {
+  static const int maxProfilePhotos = 5;
+  static const int freeLikesRolling24h = 100;
+  static const String adultsOnly = '18+';
+  static const List<String> bottomTabs = ['Discover', 'Feed', 'Map', 'Likes', 'Messages'];
+  static const Map<String, String> premiumPrices = {
+    'Monthly': '₹500',
+    '6 months': '₹2,500',
+    'Yearly': '₹4,000',
+  };
+}
