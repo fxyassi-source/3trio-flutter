28465e90a4a996325a2a5b0247cfc89df688c4e8
fxyassi-source <fxyassi@gmail.com>
2026-09-03 14:50:07 +0530

    Add protected chat and call rules

---
+++ lib/core/chat_rules.dart
@@ -0,0 +1,6 @@
+class ChatRules {
+  static const bool oneViewMediaExpiresAfterOpen = true;
+  static const bool screenshotProtectionBestEffort = true;
+  static const bool callsRecordedByDefault = false;
+  static const String secureChatLabel = 'Secure Chat';
+}
