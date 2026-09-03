14ac8215d5360cfa6f53460f6de37bb130f32795
fxyassi-source <fxyassi@gmail.com>
2026-09-03 14:48:06 +0530

    Add privacy and safety policy constants

---
+++ lib/core/privacy_policy.dart
@@ -0,0 +1,11 @@
+class PrivacyPolicy {
+  static const neverExposeExactGps = true;
+  static const privatePhotosExcludedFromDiscovery = true;
+  static const governmentIdPrivate = true;
+  static const oneViewCannotReopen = true;
+  static const screenshotProtectionBestEffort = true;
+  static const noUniversalScreenshotClaim = true;
+
+  static String nearbyNotification() => 'Someone nearby liked you ❤️';
+  static String crossedPaths() => 'You crossed paths in this area';
+}
