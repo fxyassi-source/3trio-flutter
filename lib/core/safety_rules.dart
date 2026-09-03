403c7f88419a418b20eb3682b2c47e729cbb9d10
fxyassi-source <fxyassi@gmail.com>
2026-09-03 14:50:17 +0530

    Add location and verification safety rules

---
+++ lib/core/safety_rules.dart
@@ -0,0 +1,7 @@
+class SafetyRules {
+  static const String nearbyLikeMessage = 'Someone nearby liked you ❤️';
+  static const String crossedPathsMessage = 'You crossed paths in this area';
+  static const bool exactGpsNeverPublic = true;
+  static const bool liveMovementTrailNeverPublic = true;
+  static const bool verificationBadgesRemainSeparate = true;
+}
