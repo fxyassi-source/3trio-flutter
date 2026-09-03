b83347b3e2dc4dff9fa63b1479d4d53760f1aeda
fxyassi-source <fxyassi@gmail.com>
2026-09-03 14:50:11 +0530

    Add exact premium entitlement rules

---
+++ lib/core/premium_rules.dart
@@ -0,0 +1,8 @@
+class PremiumRules {
+  static const int monthlyInr = 500;
+  static const int sixMonthsInr = 2500;
+  static const int yearlyInr = 4000;
+  static const int freeLikesRolling24h = 100;
+  static const bool premiumUnlimitedLikes = true;
+  static const bool premiumSeeWhoLiked = true;
+}
