e4c30ca54b13464e2f2216f3621f59ac7b4ac0bc
fxyassi-source <fxyassi@gmail.com>
2026-09-03 14:51:02 +0530

    Add Migo-style activity safety rules

---
+++ lib/core/activity_rules.dart
@@ -0,0 +1,6 @@
+class ActivityRules {
+  static const categories = <String>['Restaurants', 'Cafés', 'Bars', 'Clubs', 'Outdoors', 'Sports', 'Culture', 'Events', 'Entertainment'];
+  static const bool exactActivityLocationHidden = true;
+  static const bool participantChatAfterJoin = true;
+  static const bool organizerControlsReportBlock = true;
+}
